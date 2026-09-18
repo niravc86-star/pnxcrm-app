4c7cfe2aee9e58d3be4dee919c4190c5800042c2
niravc86-star <niravc86@gmail.com>
2026-09-18 17:35:59 +0530

    Implement custom back navigation in MainActivity
    Override onBackPressed to handle web view navigation.

--- android/app/src/main/java/com/profitnx/crm/MainActivity.java
+++ android/app/src/main/java/com/profitnx/crm/MainActivity.java
@@ -2,4 +2,13 @@ package com.profitnx.crm;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onBackPressed() {
+        if (getBridge() != null && getBridge().getWebView().canGoBack()) {
+            getBridge().getWebView().goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
